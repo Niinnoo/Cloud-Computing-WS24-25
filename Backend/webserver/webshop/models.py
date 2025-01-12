2faf98ebef1787775c278a887909b959e7b840c9
ValDavSto <vdstolz@gmail.com>
2025-01-12 17:09:38 +0100

    Added first sample data into the database for testing

--- Backend/webserver/webshop/models.py
+++ Backend/webserver/webshop/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
-class ProductCategories(models.Model):
+class ProductCategory(models.Model):
     category = models.CharField(max_length=100, unique=True)
 
-class ProductData(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     short_description = models.CharField(max_length=200)
     long_description = models.CharField(max_length=500)
-    category = models.ForeignKey(ProductCategories, on_delete=models.CASCADE, related_name='products')
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE, related_name='products')
 
 
