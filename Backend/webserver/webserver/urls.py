5333d7672c5cb002ca73bd2599eea61ec3b79c80
Nino <nino.zoric@yahoo.de>
2025-01-23 18:37:43 +0100

    Update urls.py
    new url for email service

--- Backend/webserver/webserver/urls.py
+++ Backend/webserver/webserver/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('webshop/', include("webshop.urls"))
+    path('webshop/', include("webshop.urls")),
+    path('emailservice/', include('emailservice.urls'))
 ]
