38269f7b191b78c5f6c319091cb870bc027405cf
Clemens <59542939+kraideblaich@users.noreply.github.com>
2025-01-29 14:45:56 +0100

    Database switched to AWS RDS MySQL

--- Backend/webserver/webserver/settings.py
+++ Backend/webserver/webserver/settings.py
@@ -78,10 +78,21 @@ WSGI_APPLICATION = 'webserver.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ccwebshopdb',
+        'USER': 'dbuser',
+        'PASSWORD': '***',
+        'HOST': 'ccwebshopdb.cfhfs5nbkdiz.us-east-1.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
