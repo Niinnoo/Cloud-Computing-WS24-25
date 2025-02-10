1c1f0cfd54217c318b98191f8e25b961206d5cf4
ValDavSto <vdstolz@gmail.com>
2025-02-10 12:00:02 +0100

    Forgot response

--- Backend/webserver/webshop/views/TaskView.py
+++ Backend/webserver/webshop/views/TaskView.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from ..tasks import low_stock, update_order_status
 
 
@@ -8,3 +9,5 @@ class TaskView(APIView):
     def get(self, request):
         low_stock(10) # checks inventory for products with quantity below 10 and sends an email to notify the shop owner 
         update_order_status() # simulates activity of delivery by setting all products to the next step
+        
+        return Response(status=status.HTTP_200_OK)
